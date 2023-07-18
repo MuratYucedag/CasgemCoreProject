05c02af52e1bcc1570f89bf5aaf379429261353b
MuratYucedag <yucedagmurat23@gmail.com>
2023-07-18 10:12:36 +0300

    ....

---
+++ Pizzapan.PresentationLayer/ViewComponents/Default/_FeaturePartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Pizzapan.PresentationLayer.ViewComponents.Default
+{
+    public class _FeaturePartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.title1 = "Her Gün Pizza Yiyin";
+            ViewBag.title2 = "Sevdiğiniz Pizzaları Paylaşın";
+            return View();
+        }
+    }
+}
